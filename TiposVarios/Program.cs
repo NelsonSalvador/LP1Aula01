c79aa022a70de5dd106ce6812c0a17a9729318d9
NelsonSalvador <nelsonsalvadorzx@gmail.com>
2020-02-27 17:20:54 +0000

    Print de char unicode

--- TiposVarios/Program.cs
+++ TiposVarios/Program.cs
@@ -11,10 +11,13 @@ namespace TiposVarios
             int p = 3;
             float a = 4.56f;
             double r = 5.67;
+
             char smile = '\u2661';
+            
             Console.WriteLine(p);
             Console.WriteLine(a);
             Console.WriteLine(r);
+
             Console.Write(smile);
             Console.Write(smile);
             Console.Write(smile);
