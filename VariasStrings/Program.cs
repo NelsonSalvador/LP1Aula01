b14bf3153b180b3fc2d51e6069fcf25727d7c5e5
NelsonSalvador <nelsonsalvadorzx@gmail.com>
2020-02-27 17:51:49 +0000

    Mais variaveis do tipo string

--- VariasStrings/Program.cs
+++ VariasStrings/Program.cs
@@ -6,10 +6,18 @@ namespace VariasStrings
     {
         static void Main(string[] args)
         {
+            int i = 45;
+            string e = "a " + 2;
+            string w = String.Format("O valor é de : {0}", 45); 
+            string h = $"Numero {i}";
             string s = "Um tab\t e um copyright\u03A9";
             string r = @"Nao e’ um codigo Unicode \u1234, nao e’ nova linha \n";
             Console.WriteLine(s);
             Console.WriteLine(r);
+            Console.WriteLine(e);
+            Console.WriteLine(w);
+            Console.WriteLine(h);
+            Console.WriteLine(@"tenho {0}", i);
         }
     }
 }
