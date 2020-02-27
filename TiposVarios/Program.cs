6346120b47f6eef5fc41d468f6a047b8cd545ed5
NelsonSalvador <nelsonsalvadorzx@gmail.com>
2020-02-27 17:20:11 +0000

    Print de variaveis inteiras

---
+++ TiposVarios/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Text;
+
+namespace TiposVarios
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.OutputEncoding = Encoding.UTF8;
+            int p = 3;
+            float a = 4.56f;
+            double r = 5.67;
+            char smile = '\u2661';
+            Console.WriteLine(p);
+            Console.WriteLine(a);
+            Console.WriteLine(r);
+            Console.Write(smile);
+            Console.Write(smile);
+            Console.Write(smile);
+            Console.Write(smile);
+
+        }
+    }
+}
