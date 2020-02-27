914d5af6de4c4606cbc3ac8c7961dcccd33fc515
NelsonSalvador <nelsonsalvadorzx@gmail.com>
2020-02-27 16:15:45 +0000

    Documentação XML

--- Argumentos/Program.cs
+++ Argumentos/Program.cs
@@ -2,8 +2,17 @@
 
 namespace Argumentos
 {
+    /// <summary>
+    /// Prints on the console the argumets passed
+    /// </summary>
     class Program
     {
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="args"></param>
+        /// Stores the arguments passed
+        /// </param>
         static void Main(string[] args)
         {
             for (int i = 0; i < args.Length; i++)
